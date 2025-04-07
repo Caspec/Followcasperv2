90616f3dd1261135dfb860362d6da60c146da5ab
Caspec <quii2000@yahoo.dk>
2025-04-07 22:46:01 +0200

    chore: update funfacts and remove unused d.ts files

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -72,6 +72,7 @@
 import { ref } from 'vue';
 import WorkExperience from '@/components/WorkExperience.vue';
 import profileImageSrc from '@/assets/me.png';
+import { funFacts } from '@/content/funFacts';
 
 const name = ref('Casper');
 const profileImage = ref(profileImageSrc);
@@ -84,39 +85,6 @@ const skills = ref([
   { name: 'Laravel', icon: 'bi bi-layers' },
   { name: 'Go', icon: 'bi bi-globe' },
   { name: 'More...', icon: 'bi bi-three-dots' },
-]);
-
-const funFacts = ref([
-  {
-    title: '🎣 Fishing Enthusiast',
-    description:
-      'In my spare time, I love to fish and unwind by the water. It’s a peaceful hobby that helps me recharge and reconnect with nature. My personal record for rainbow trout is 5kg!',
-  },
-  {
-    title: '🚲 Bicycle Mechanic Background',
-    description:
-      'Before diving into tech, I worked as a bicycle mechanic (known as "cykelmontør" in Danish). This hands-on experience taught me attention to detail, problem-solving, and the satisfaction of fixing things with my own two hands.',
-  },
-  {
-    title: '🍲 Master of Mørbradgryde',
-    description:
-      'One of my culinary specialties is making a delicious Mørbradgryde, a traditional Danish pork stew. It’s a family favorite and I love experimenting with different flavors to perfect it!',
-  },
-  {
-    title: '🗣️ Once Had a Long Conversation',
-    description:
-      'While working at Fakta, I had the chance to talk to actor Ulf Pilgaard for an extended period of time. It was an unforgettable experience, and he was friendly and eager to chat!',
-  },
-  {
-    title: '📺 Old Danish Movies & Actors',
-    description:
-      'I have a soft spot for classic Danish films and series. I enjoy learning about the stories behind iconic actors, especially through shows like "Her Er Dit Liv". Also, I’m a fan of shows like "Nak & Æd", which never fail to entertain me.',
-  },
-  {
-    title: '🏆 Sports',
-    description:
-      'I am passionate about watching sports, especially cycling, soccer, handball, and snooker, both live in person and on TV, especially competitions that keep me glued to the screen!',
-  },
 ]);
 </script>
 
