18fbac570a20345a4c2a0f70dafbea0ac48ded65
Caspec <quii2000@yahoo.dk>
2025-04-22 07:46:30 +0200

    feat: add uppercase to monopoly

---
+++ src/components/games/Monopoly/MonopolyDice.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="text-center">
+    <button class="btn btn-success" @click="roll">🎲 Roll Dice</button>
+    <p class="mt-3">
+      Dice: <strong>{{ value }}</strong>
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits<{
+  (e: 'rollDice', value: number): void;
+}>();
+
+defineProps<{
+  value: number;
+}>();
+
+const roll = () => {
+  const rollDice = Math.floor(Math.random() * 6) + 1;
+  emit('rollDice', rollDice);
+};
+</script>
